b0f9a7069cb8dcf0e5f4ef8438c7343432a192e4
Isgandar Valizada <isgandar.valizada@de.ibm.com>
2021-03-24 16:28:12 +0100

    correction in persister inheritance in COS

--- app/models/manageiq/providers/ibm_cloud/inventory/persister.rb
+++ app/models/manageiq/providers/ibm_cloud/inventory/persister.rb
@@ -1,4 +1,4 @@
-class ManageIQ::Providers::IbmCloud::Inventory::Persister < ManageIQ::Providers::Inventory::Persister::ObjectStorage
+class ManageIQ::Providers::IbmCloud::Inventory::Persister < ManageIQ::Providers::Inventory::Persister
   require_nested :ObjectStorage
   require_nested :PowerVirtualServers
   require_nested :VPC
