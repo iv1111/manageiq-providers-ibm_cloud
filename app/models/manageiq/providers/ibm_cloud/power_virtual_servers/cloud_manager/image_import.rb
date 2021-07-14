af6f4f35c90179608d3ce2dbc78649e52be6d35f
Isgandar Valizada <isgandar.valizada@de.ibm.com>
2021-07-14 17:52:19 +0200

    using Active "ActiveSupport::Concern" for static private method declaration

--- app/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/image_import.rb
+++ app/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/image_import.rb
@@ -1,26 +1,29 @@
 module ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::ImageImport
-    def encrypt_with_aes(creds)
-      cipher = OpenSSL::Cipher.new('aes-256-cbc')
-      cipher.encrypt
+  extend ActiveSupport::Concern
 
-      key  = Base64.strict_encode64(cipher.random_key)
-      iv   = Base64.strict_encode64(cipher.random_iv)
-      encr = Base64.strict_encode64(cipher.update(creds.to_json) + cipher.final)
+  included do
+    private_class_method def self.encrypt_with_aes(creds)
+        cipher = OpenSSL::Cipher.new('aes-256-cbc')
+        cipher.encrypt
 
-      [encr, key, iv]
+        key  = Base64.strict_encode64(cipher.random_key)
+        iv   = Base64.strict_encode64(cipher.random_iv)
+        encr = Base64.strict_encode64(cipher.update(creds.to_json) + cipher.final)
+
+        [encr, key, iv]
     end
 
-    def miq_img_by_ids(provider_id, image_id)
+    private_class_method def self.miq_img_by_ids(provider_id, image_id)
       powervc = ExtManagementSystem.find(provider_id)
       powervc.get_image_info(image_id)
     end
 
-    def cos_creds(provider_id)
+    private_class_method def self.cos_creds(provider_id)
       cos = ExtManagementSystem.find(provider_id)
       cos.cos_creds
     end
 
-    def node_creds(provider_id)
+    private_class_method def self.node_creds(provider_id)
       powervc = ExtManagementSystem.find(provider_id)
       endp = powervc.node_endpoint
       auth = powervc.node_auth
@@ -28,11 +31,12 @@ module ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::ImageIm
       return endp.hostname, auth.userid, auth.password
     end
 
-    def image_ems_ref(bucket_id)
+    private_class_method def self.image_ems_ref(bucket_id)
       MiqTemplate.find(bucket_id).uid_ems
     end
 
-    def bucket_name(bucket_id)
+    private_class_method def self.bucket_name(bucket_id)
       CloudObjectStoreContainer.find(bucket_id).name
     end
+  end
 end
